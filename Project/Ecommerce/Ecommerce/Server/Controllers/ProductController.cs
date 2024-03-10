199b1e7464cedd3a5859ca73966ae2282a02082f
AleksandraAntonijevic <149418568+AleksandraAntonijevic@users.noreply.github.com>
2024-03-10 12:11:18 +0100

    novo

---
+++ Project/Ecommerce/Ecommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Ecommerce.Server.Repositories.Interfaces;
+using Ecommerce.Shared.DTO;
+using Ecommerce.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepo productRepo;
+        public ProductController(IProductRepo productRepo)
+        {
+            this.productRepo = productRepo;
+        }
+
+        [HttpPost("Add-Product")]
+        public async Task<ActionResult<ServiceModel>> AddProduct(Product NewProduct)
+        {
+            return Ok(await productRepo.AddProduct(NewProduct));
+        }
+
+        [HttpGet("Get-Products")]
+        public async Task<ActionResult<ServiceModel>> GetProducts()
+        {
+            return Ok(await productRepo.GetProducts());
+        }
+
+        [HttpGet("Get-Product/{ProductId:int}")]
+        public async Task<ActionResult<ServiceModel>> GetProduct(int ProductId)
+        {
+            return Ok(await productRepo.GetProduct(ProductId));
+        }
+    }
+}
